f9c4d502752f116e037e1f421155746414c3b578
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2023-10-31 21:27:42 -0300

    chore: seeding database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const{data} = useSession()
+  
   return (
     <div>
-      <h1 className="">Home</h1>
+    
     </div>
   );
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,27 @@
-import { MenuIcon, ShoppingCartIcon, LogInIcon, PercentIcon, ListOrderedIcon } from "lucide-react";
+"use client";
+import {
+  MenuIcon,
+  ShoppingCartIcon,
+  LogInIcon,
+  PercentIcon,
+  ListOrderedIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
-
+import { signIn, useSession, signOut } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 export const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -13,28 +31,62 @@ export const Header = () => {
           </Button>
         </SheetTrigger>
         <SheetContent side="left">
-            <SheetHeader className="text-left text-lg font-semibold">
-                Menu
-            </SheetHeader>
-            <div className="mt-2 flex flex-col gap-2">
-                <Button className="w-full justify-start gap-2" variant="outline">
-                    <LogInIcon size={16}/>
-                    Fazer Login
-                </Button>
-                <Button className="w-full justify-start gap-2" variant="outline">
-                    <PercentIcon size={16}/>
-                    Ofertas
-                </Button>
-                <Button className="w-full justify-start gap-2" variant="outline">
-                    <ListOrderedIcon size={16}/>
-                    Catálogo
-                </Button>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          {status === "authenticated" && data?.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+                <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
+                </div>
+              </div>
+              <Separator />
             </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-2">
+            {status == "unauthenticated" && (
+              <Button
+                className="w-full justify-start gap-2"
+                variant="outline"
+                onClick={handleLoginClick}
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+            )}
+            {status == "authenticated" && (
+              <Button
+                className="w-full justify-start gap-2"
+                variant="outline"
+                onClick={handleLogoutClick}
+              >
+                <LogInIcon size={16} />
+                Fazer Logout
+              </Button>
+            )}
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
         </SheetContent>
-          <h1 className="text-lg font-semibold">
-            <span className="text-primary">FSW</span>
-            Store
-          </h1>
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FSW</span>
+          Store
+        </h1>
       </Sheet>
 
       <Button size="icon" variant="outline">
