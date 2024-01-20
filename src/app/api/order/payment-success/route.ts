e3ab061c966516f3d7f082678713b39523e38bed
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2024-01-20 17:35:50 -0300

    feat: add checkout stripe webhook

---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (request: Request) => {
+  const signature = await request.headers.get("stripe-signature");
+
+  if (!signature) {
+    return NextResponse.error();
+  }
+  const text = await request.text();
+  const event = await stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(event.data.object.id,{
+        expand:['line_items'],
+    })
+    const line_items = sessionWithLineItems.line_items
+  }
+
+  return NextResponse.json({received:true})
+};
